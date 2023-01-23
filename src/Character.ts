124b58feabfae2988c8837e8e0f3951e81936675
rafaelmedeiross <rafael.medeiros@msn.com>
2023-01-23 20:58:59 -0300

    req08

---
+++ src/Character.ts
@@ -0,0 +1,35 @@
+// import Archetype from "./Archetypes";
+// import Energy from "./Energy";
+// import Race from "./Races";
+// import Elf from "./Races";
+
+import Fighter from "./Fighter";
+
+export default class Character implements Fighter {
+//   private _race: Race;
+//   private _archetype: Archetype;
+//   private _maxLifePoints: number;
+//   private _lifePoints: number;
+//   private _strength: number;
+//   private _defense: number;
+//   private _dexterity: number;
+//   private _energy: Energy;
+//   private _name: string;
+
+//   constructor(name: string) {
+//     this._name = name;
+//     this._dexterity = 7;
+//     this._race = new Elf(name, 7);
+//     this._maxLifePoints = 
+// }
+
+// get name(): string { return this._name; }
+
+// get dexterity(): number { return this._dexterity; }
+
+// static createdRacesInstances(): number {
+//     throw new Error('Not implemented');
+// }
+
+// abstract get maxLifePoints(): number;
+}
