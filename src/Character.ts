1a784b0d388a99aca06c4b62315365f48ce25547
rafaelmedeiross <rafael.medeiros@msn.com>
2023-01-24 21:10:50 -0300

    req07

--- src/Character.ts
+++ src/Character.ts
@@ -1,35 +1,67 @@
-// import Archetype from "./Archetypes";
-// import Energy from "./Energy";
-// import Race from "./Races";
-// import Elf from "./Races";
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+import Fighter from './Fighter';
 
-// import Fighter from "./Fighter";
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
 
-// export default class Character implements Fighter {
-//   private _race: Race;
-//   private _archetype: Archetype;
-//   private _maxLifePoints: number;
-//   private _lifePoints: number;
-//   private _strength: number;
-//   private _defense: number;
-//   private _dexterity: number;
-//   private _energy: Energy;
-//   private _name: string;
+  constructor(name: string) {
+    this._archetype = new Mage(name);
+    this._dexterity = getRandomInt(0, 10);
+    this._defense = getRandomInt(0, 10);
+    this._strength = getRandomInt(0, 10);
+    this._race = new Elf(name, this._dexterity);
+    this._maxLifePoints = this._race.maxLifePoints / 2;
+    this._lifePoints = this._maxLifePoints;
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(0, 10),
+    };
+  }
+    
+  get race(): Race { return this._race; }
+  get archetype(): Archetype { return this._archetype; }
+  get lifePoints(): number { return this._lifePoints; }
+  get strength(): number { return this._strength; }
+  get defense(): number { return this._defense; }
+  get dexterity(): number { return this._dexterity; }
+  get energy(): Energy { 
+    return {
+      type_: this._energy.type_,
+      amount: this._energy.amount,
+    };
+  }
 
-//   constructor(name: string) {
-//     this._name = name;
-//     this._dexterity = 7;
-//     this._race = new Elf(name, 7);
-//     this._maxLifePoints = 
-// }
+  receiveDamage(attackPoints: number) {
+    const damage = attackPoints - this._defense;
+    if (damage > 0) this._lifePoints -= damage;
+    if (damage <= 0) this._lifePoints -= 1;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;    
+  }
 
-// get name(): string { return this._name; }
+  attack(enemy: Fighter): void { 
+    enemy.receiveDamage(this._strength); 
+  }
 
-// get dexterity(): number { return this._dexterity; }
-
-// static createdRacesInstances(): number {
-//     throw new Error('Not implemented');
-// }
-
-// abstract get maxLifePoints(): number;
-// }
+  levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._lifePoints = this._maxLifePoints;
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+  }
+}
